1ed918ae72bd7fe42a7452117b0a061ca5c8610e
gerardomuela96 <gerardomuela96@gmail.com>
2017-11-26 20:29:27 -0700

    Initial commit

---
+++ Board.hs
@@ -0,0 +1,48 @@
+module Board where
+
+rowMaker x = take x (repeat 0)
+
+colMaker n x = if n > 0 then rowMaker x:colMaker(n-1) x
+          else []
+
+col x bd s = if s <= (size bd) then (row s bd !! (x-1)):col x bd (s+1)
+             else []
+
+mkBoard n = if n > 0 then rowMaker n:colMaker (n-1) n
+            else []
+
+mkPlayer = 1
+
+mkOpponent = 2
+
+size bd = length bd
+
+row y bd = bd !! (y-1)
+
+column x bd = (row 1 bd !! (x-1)):col x bd 2
+
+mark x y bd p = take (y-1) bd++[take (x-1) (row y bd)++[p]++drop x (row y bd)]++drop y bd
+
+isEmpty x y bd = ((row y bd) !! (x-1) == 0)
+
+isMarked x y bd = ((row y bd) !! (x-1) /= 0)
+
+isMarkedBy x y bd p = ((row y bd) !! (x-1) == p)
+
+marker x y board = (row y board) !! (x-1)
+
+isFull bd = product [minimum x | x <-bd] /= 0
+
+isWonBy bd p = True
+
+isDraw bd = (isFull bd) && (not(isWonBy bd mkPlayer) || not(isWonBy bd mkOpponent))
+
+isGameOver bd = (isDraw bd) || (isWonBy bd mkPlayer) || (isWonBy bd mkOpponent)
+
+boardToStr playerToChar bd = putStr (iterateBoard playerToChar 1 1 bd)
+
+iterateBoard playerToChar y x bd = if x <= (size bd) then
+                                      if (marker x y bd) /= 0 then (playerToChar (marker x y bd))++(iterateBoard playerToChar y (x+1) bd)
+				      else " . "++iterateBoard playerToChar y (x+1) bd
+                                   else if y < size bd then "\n"++iterateBoard playerToChar (y+1) 1 bd
+				   else ""
---
+++ Main.hs
@@ -0,0 +1,6 @@
+import Board
+
+playerToChar p = if p == 1 then " O "
+                 else " X "
+
+-- readXY bd p =
